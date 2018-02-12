ab0d918dcb075a3fa4af88e2ebb03c728d6b7fbc
jsadler2 <jeffrey.sadler2@gmail.com>
2018-02-12 13:41:55 -0500

    styled legend dumb vs smart

--- simple_model/data/plot_active_vs_passive.py
+++ simple_model/data/plot_active_vs_passive.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rc
 
-df = pd.read_csv("dumb_vs_smart.csv", index_col=0, skiprows=1, names=["Node Depth-Active Control", 
-    "Node Depth-Passive Control", "Node Flooding-Passive Control"])
+# activate latex text rendering
+rc('text', usetex=True)
+
+df = pd.read_csv("dumb_vs_smart.csv", index_col=0, skiprows=1, names=[r"DS Node Depth (ft)-\textit{Active}", 
+    r"DS Node Depth (ft)-\textit{Passive}", r"DS Node Flooding (cfs)-\textit{Passive}"])
 df.reset_index(inplace=True)
 df.set_index(df.index/12., inplace=True)
 font_size = 12
@@ -12,7 +16,7 @@ passive_color = "0.55"
 lines[1].set_color(passive_color)
 lines[2].set_color(passive_color)
 lines[2].set_linestyle("--")
-lgd = ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.20), ncol=2, fontsize=font_size)
+lgd = ax.legend(loc='lower left', bbox_to_anchor=(0, 1), ncol=1, fontsize=font_size)
 ax.set_xlabel("Time elapsed (hr)", fontsize=font_size)
 fig = plt.gcf()
 fig.savefig("dumb_vs_smart",  bbox_inches="tight")
