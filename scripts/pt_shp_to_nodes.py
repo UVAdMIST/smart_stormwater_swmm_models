467dffbe5235021c6ea1a7f07d9fd15a8781aacb
jsadler2 <jeffrey.sadler2@gmail.com>
2018-05-24 10:24:23 -0400

    writes xsec section from shp

--- scripts/pt_shp_to_nodes.py
+++ scripts/pt_shp_to_nodes.py
@@ -1,8 +1,13 @@
+import numpy as np
 from swmmio import swmmio
 from swmmio.utils.modify_model import replace_inp_section
 from get_contributing_area import get_upstream_nodes, get_upstream_conduits
 import pandas as pd
 from shutil import copyfile
+
+def make_new_df(index, cols, data):
+    df = pd.DataFrame(index=index, data=dict(zip(cols, data)))
+    return df[cols]
 
 # read in template model
 template_inp = "../brambleton/template.inp"
@@ -32,33 +37,38 @@ us_cons_ids = get_upstream_conduits(outlet_id, pdf, in_col_name=us_node_col_name
 us_cons = pdf.loc[us_cons_ids]
 
 # convert the data into the format of the inp file tables
-jxns_nw = pd.DataFrame(index=us_node_data.index,
-                       data={
-                           'InvertElev':us_node_data['Invert_Ele'],
-                           'MaxDepth':us_node_data['Rim_Elevat'],
-                           'SurchargedDepth':0,
-                           'PondedArea':0
-                           }
-                      )
+# make JUNCTIONS dataframe
+# columns list so that we can have the columns in a specific order
+jxns_cols = ['InvertElev', 'MaxDepth', 'SurchargedDepth', 'PondedArea'] 
+# data for the columns
+jxns_data = [us_node_data['Invert_Ele'], us_node_data['Rim_Elevat'], 0, 0]
+jxns_nw = make_new_df(us_node_data.index, jxns_cols, jxns_data)
 
-coord_nw = pd.DataFrame(index=us_node_data.index,
-                        data={
-                            'X-Coord':us_node_data['Easting'],
-                            'Y-Coord':us_node_data['Northing']
-                            }
-                       )
+# make COORDINATES dataframe
+coord_cols = ['X-Coord', 'Y-Coord']
+coord_data = [us_node_data['Easting'], us_node_data['Northing']]
+coord_nw = make_new_df(us_node_data.index, coord_cols, coord_data)
 
-conduits_columns = ['InletNode', 'OutletNode', 'Length', 'ManningN', 'InletOffset', 'OutletOffset',
+# make CONDUITS dataframe
+conduits_cols = ['InletNode', 'OutletNode', 'Length', 'ManningN', 'InletOffset', 'OutletOffset',
                    'InitFlow', 'MaxFlow'] 
 conduits_data = [us_cons[us_node_col_name], us_cons[ds_node_col_name], us_cons['Pipe_Lengt'], 
                  0.015, 0, 0, 0, 0 ]
-conduits_nw = pd.DataFrame(index=us_cons.index,
-                           data=dict(zip(conduits_columns, conduits_data))
-                          )
-conduits_nw = conduits_nw[conduits_columns]
+conduits_nw = make_new_df(us_cons.index, conduits_cols, conduits_data)
 
+# make XSECTION dataframe
+xsec_cols = ['Shape', 'Geom1', 'Geom2', 'Geom3', 'Geom4', 'Barrels', 'Culvert']
+# translate pipe geom
+xsec_geom_sel = [us_cons['Pipe_Geome']=='CR', us_cons['Pipe_Geome']=='BX', 
+                 us_cons['Pipe_Geome']=='AR', us_cons['Pipe_Geome']=='EL']
+xsec_geom_vals = ['CIRCULAR', 'RECT_CLOSED', 'ARCH', 'HORIZ_ELLIPSE']
+xsec_data = [np.select(xsec_geom_sel, xsec_geom_vals), 
+             us_cons['Horizontal']/12., us_cons['Vertical_D']/12., 0, 0, 1, ''] 
+             
+xsec_nw = make_new_df(us_cons.index, xsec_cols, xsec_data)
 
 # replace the template model junctions with info from the node shapefile
 replace_inp_section(target_inp, '[JUNCTIONS]', jxns_nw)
 replace_inp_section(target_inp, '[COORDINATES]', coord_nw)
 replace_inp_section(target_inp, '[CONDUITS]', conduits_nw)
+replace_inp_section(target_inp, '[XSECTIONS]', xsec_nw)
